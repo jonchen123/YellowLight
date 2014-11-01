47beac08b231d5ef6bdbd4985e0e900cf3f95f3b
jonchen123 <jonchen123@users.noreply.github.com>
2014-10-31 23:11:34 -0400

    Update GraphicsSim.java

--- YellowLight/src/GraphicsSim.java
+++ YellowLight/src/GraphicsSim.java
@@ -1,3 +1,11 @@
-public class GraphicSim
+import javax.swing.JFrame;
+
+public class GraphicSim   //tester
 {
-  
+  public static void main(String[] args)
+  {
+    JFrame appFrame = new YellowLightPanel();
+    appFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    appFrame.show();
+  }
+}
