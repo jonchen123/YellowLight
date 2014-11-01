1bdd233813f5d91ce083ef89492f454d6f2d2ece
jonchen123 <jonchen123@users.noreply.github.com>
2014-10-31 23:17:35 -0400

    Update YellowLightPanel.java

--- YellowLight/src/YellowLightPanel.java
+++ YellowLight/src/YellowLightPanel.java
@@ -1,7 +1,28 @@
-public class YellowLightPanel
+import java.awt.Graphics2D;
+import java.awt.geom.Rectangle2D;
+import java.awt.Color;
+import javax.swing.*;
+
+public class YellowLightPanel extends JPanel
 {
+  private static int PANEL_WIDTH = 500;
+  private static int PANEL_HEIGHT = 500;
+  
   public YellowLightPanel()
   {
-    
+    setPreferredSize(new Dimension(PANEL_WIDTH, PANEL_HEIGHT));
+  }
+  
+  private void draw(Graphics2D g2)
+  {
+    //draw cars traveling up and down (blue)
+    //draw cars traveling left and right  (red)
+  }
+  
+  public void paintComponent(Graphics g)
+  {
+    super.paintComponent(g);
+    Graphics2D g2 = (Graphics2D) g;
+    draw(g2);
   }
 }
